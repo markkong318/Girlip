7ec966c0e691a9681a31fbd8f983e918057154d7
mark <mark@rgames.jp>
2017-08-30 15:40:43 +0900

    processing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import CharacterList from './CharacterList';
-import CharacterProfile from './CharacterProfile';
-import CharacterWorld from './CharacterWorld';
-import CharacterMovies from './CharacterMovies';
+import React from 'react'
+import { browserHistory, Router } from 'react-router'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
 
-const App = () =>
-  <div className='container'>
-    <div className='row'>
-      <CharacterList />
-      <CharacterProfile />
-    </div>
-    <div className='row'>
-      <CharacterWorld />
-      <CharacterMovies />
-    </div>
-  </div>;
+class App extends React.Component {
+  static propTypes = {
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired,
+  }
 
-export default App;
+  shouldComponentUpdate () {
+    return false
+  }
+
+  render () {
+    return (
+      <Provider store={this.props.store}>
+        <div style={{ height: '100%' }}>
+          <Router history={browserHistory} children={this.props.routes} />
+        </div>
+      </Provider>
+    )
+  }
+}
+
+export default App
--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import characters from './characters';
-import character from './character';
-
-export default combineReducers({
-  characters,
-  character,
-});
